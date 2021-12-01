0d933e8df16c54455ffffe6a603f097697accf10
user42 <ael-khat@student.42.fr>
2021-12-01 23:12:37 +0100

    strlcat

---
+++ strlcat/ft_strlcat.c
@@ -0,0 +1,29 @@
+#include "../libft.h"
+
+size_t	ft_strlcat(char	*dest, const	char *src, size_t size)
+{
+	int	d_len;
+	int	s_len;
+	int	gap;
+	int	i;
+	int	offset;
+
+	d_len = ft_strlen(dest);
+	s_len = ft_strlen(src);
+	offset = d_len;
+	gap = size - (offset + 1);
+	i = 0;
+	while (gap >= 1 && src[i])
+	{	
+		dest[offset] = src[i];
+		i ++;
+		gap --;
+		offset++;
+	 }
+	dest[offset] = '\0';	
+	if (gap  >= 0)
+		return (d_len + s_len);
+	else
+		return (size + s_len);
+}
+
