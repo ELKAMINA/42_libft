652e232fea9974f6a88889e2453c640b7b22d669
Amina EL KHATTABI <ael-khat@e3r6p11.42.fr>
2021-12-08 20:39:54 +0100

    jusqua itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ael-khat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 16:33:54 by ael-khat          #+#    #+#             */
-/*   Updated: 2021/12/06 16:35:15 by ael-khat         ###   ########.fr       */
+/*   Updated: 2021/12/08 20:39:31 by ael-khat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,21 @@ char	*ft_itoa(int n)
 	long	n_long;
 
 	n_long = n;
-	index_tab = sizeof_myint(n_long) - 1;
-	tab = (char *)malloc((sizeof_myint(n) * sizeof(char) + 1));
+	index_tab = sizeof_myint(n_long);
+	tab = (char *)malloc((sizeof_myint(n) + 1) * sizeof(char));
 	if (tab == NULL)
 		return (NULL);
+	tab[index_tab] = '\0';
 	if (n_long < 0)
 	{
 		tab[0] = '-';
 		n_long *= -1;
 	}
-	while (n_long >= 1)
+	else if (n_long == 0)
+		tab[index_tab -1] = '0';
+	while (n_long != 0)
 	{
-		tab[index_tab] = (n_long % 10) + 48;
+		tab[index_tab - 1] = (n_long % 10) + 48;
 		n_long /= 10;
 		index_tab --;
 	}
